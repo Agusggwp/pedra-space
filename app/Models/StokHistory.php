0bb27ae532558d9ab05fa88c79e7337740bc50e1
Agusggwp <dxnos123@gmail.com>
2025-12-14 11:52:59 +0800

    feat: Update StokHistory model to include soft deletes for related Produk

--- app/Models/StokHistory.php
+++ app/Models/StokHistory.php
@@ -24,7 +24,7 @@ class StokHistory extends Model
 
     public function produk(): BelongsTo
     {
-        return $this->belongsTo(Produk::class);
+        return $this->belongsTo(Produk::class)->withTrashed();
     }
 
     public function user(): BelongsTo
